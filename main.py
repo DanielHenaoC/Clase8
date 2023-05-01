a5b25989c149918949e48e8a0bb8b63b7e1e497b
Daniel Henao <henaocardenas843@gmail.com>
2023-05-01 17:31:35 -0500

    Crea varias listas y muestra su tamaño, crea el .gitignore

---
+++ main.py
@@ -0,0 +1,30 @@
+# Hasta ahora hemos trabajado con variable que permiten almacenar un unico valor
+
+edad = 18
+Altura = 1.71
+nombre = "Daniel"
+estado = True
+
+# En python podemos utilizar una variable que permita almacerna varios tipos de datos
+
+# Lista de enteros
+lista1 = [10, 5, 3, 9]
+
+# Lista de decimales
+lista2 = [1.78, 1.23, 1.43, 1.45]
+
+# Lista de string
+lista3 = ["Lunes", "Martes", "Miercoles"]
+
+# Lista de elemento de distintos tipos
+lista4 = ["Daniel", 1.73, 18, True]
+
+if __name__ == '__main__':
+    # Cantidad de elementos que hay en una lista
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+    print()
+    lista1[2] = 3
+    print()
